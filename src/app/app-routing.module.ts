6dce18577182b71a9d1ac77bd0b691af4b0c7ba9
samundar <samundarsingh9525@gmail.com>
2024-10-14 22:43:03 +0530

    merged changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidenavComponent } from './widget/sidenav/sidenav.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { SellerDashboardComponent } from './pages/seller-dashboard/seller-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'dashboard/:id', component: SidenavComponent },
+   { path: 'products', component: ProductsPageComponent }
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
